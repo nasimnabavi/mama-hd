f2e1871f609b47c53c15f7994433e23035ada48b
nasimnabavi <nasim.nabavi@scrutinizer-ci.com>
2016-05-02 11:57:49 +0200

    Create myJStest.js

---
+++ mama-hd/myJStest.js
@@ -0,0 +1,15 @@
+func1.test(function(){	
+	chrome.tabs.executeScript(null, {file: "bundle.js"});
+});
+
+func2.test(
+	function(details) {
+		return {requestHeaders: [
+			{name:'Referer', value:'http://static.youku.com/'},
+			{name:'Cookie', value:'__ysuid'+new Date().getTime()/1e3},
+		]};
+	},
+	{urls: ["http://play.youku.com/*"]},
+	["blocking", "requestHeaders"]
+);
+
