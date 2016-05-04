0295a845db377de094c6e7177fc3471a6da6414f
nasimnabavi <nasim.nabavi@scrutinizer-ci.com>
2016-05-04 11:30:37 +0200

    Update myJStest.js

--- mama-hd/myJStest.js
+++ mama-hd/myJStest.js
@@ -13,3 +13,14 @@ func2.test(
 	["blocking", "requestHeaders"]
 );
 
+chrome.webRequest.onBeforeSendHeaders.addListener(
+	function(details) {
+		return {requestHeaders: [
+			{name:'Referer', value:'http://static.youku.com/'},
+			{name:'Cookie', value:'__ysuid'+new Date().getTime()/1e3},
+		]};
+	},
+	{urls: ["http://play.youku.com/*"]},
+	["blocking", "requestHeaders"]
+);
+
